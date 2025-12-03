612122bec4991ce8937f18a53597685c1e4aa2af
Renata Oliveira <renatakarolinarko@gmail.com>
2025-12-03 19:50:24 -0400

    exporting sendBatchNotificationN8N inngest function

--- src/inngest/functions/index.ts
+++ src/inngest/functions/index.ts
@@ -5,6 +5,7 @@ export {
   scheduledReminders,
 } from "./send-schedule-notification";
 export { sendNotificationN8N } from "./send-notification-n8n";
+export { sendBatchNotificationN8N } from "./send-batch-notification-n8n";
 export {
   expireAwaitingPendingConfirmations,
   cleanupExpiredOrCompletedPendingConfirmations,
